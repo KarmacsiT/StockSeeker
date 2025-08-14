db9d3c791f08ff65645babaf07f5ad2387f446ed
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2025-08-14 20:06:11 +0200

    Add fonts

---
+++ stock-seeker/src/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Roboto, Roboto_Flex } from "next/font/google";
+
+export const montserrat = Montserrat({ subsets: ["latin"], weight: "700" });
+export const robotoFlex = Roboto_Flex({ subsets: ["latin"], weight: "500" });
+export const roboto = Roboto({ subsets: ["latin"] });
