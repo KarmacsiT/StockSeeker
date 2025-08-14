871632a74cb199f7ee054c657327c238d0f5b9ba
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2025-08-15 01:50:42 +0200

    Extend suggested stocks

--- stock-seeker/src/app/page.tsx
+++ stock-seeker/src/app/page.tsx
@@ -2,18 +2,22 @@ import Heading from "./ui/home/heading";
 import StockCard from "./ui/home/stock-card";
 
 export default function Home() {
-	const popularStocks = [
+	const trendingStocks = [
+		{ symbol: "NVDA", name: "NVIDIA Corporation" },
 		{ symbol: "AAPL", name: "Apple Inc." },
 		{ symbol: "GOOGL", name: "Alphabet Inc." },
 		{ symbol: "AMZN", name: "Amazon.com Inc." },
 		{ symbol: "TSLA", name: "Tesla Inc." },
+		{ symbol: "IBM", name: "IBM Corporation" },
+		{ symbol: "BRK.B", name: "Berkshire Hathaway Inc." },
+		{ symbol: "MSFT", name: "Microsoft Corporation" },
 	];
 
 	return (
-		<section className="w-full max-w-2xl min-h-screen">
-			<Heading content="Popular Stocks"></Heading>
+		<section className="w-full mt-10 max-w-2xl min-h-screen">
+			<Heading content="Trending Stocks"></Heading>
 			<div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
-				{popularStocks.map((stock) => (
+				{trendingStocks.map((stock) => (
 					<StockCard key={stock.symbol} stock={stock} />
 				))}
 			</div>
