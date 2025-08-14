e26b593e3c3159113290bf16692f63f89f014694
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2025-08-14 20:08:25 +0200

    Add stock card component

---
+++ stock-seeker/src/app/ui/home/stock-card.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import router from "next/router";
+
+export default function StockCard({
+	stock: { symbol, name },
+}: {
+	stock: { symbol: string; name: string };
+}) {
+	return (
+		<button
+			onClick={() => router.push(`/search?symbol=${symbol}`)}
+			className="bg-[#1E1E1E] rounded-2xl p-5 text-center shadow-md hover:shadow-lg hover:bg-[#2A2A2A] transition"
+		>
+			<p className="text-lg font-bold text-white">{symbol}</p>
+			<p className="text-sm text-[#CFD8DC]">{name}</p>
+		</button>
+	);
+}
