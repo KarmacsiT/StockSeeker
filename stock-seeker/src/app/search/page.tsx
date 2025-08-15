7f7818ac8d1d3a359596fbad50a416ce0d5a12ae
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2025-08-15 22:22:32 +0200

    Add dynamic metadata generation to stock quote view

--- stock-seeker/src/app/search/page.tsx
+++ stock-seeker/src/app/search/page.tsx
@@ -1,6 +1,20 @@
 import { redirect } from "next/navigation";
 import { getStockQuote } from "../lib/alphaVantageApi";
+import ErrorModule from "../ui/search/errorModule";
 import QuoteOverview from "../ui/search/quoteOverview";
+
+export async function generateMetadata({
+	searchParams,
+}: {
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+	const { symbol } = await searchParams;
+
+	return {
+		title: `${symbol ?? ""} Stock Overview`,
+		description: `Overview of ${symbol} stock metrics`,
+	};
+}
 
 export default async function StockQuote({
 	searchParams,
