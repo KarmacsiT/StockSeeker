df593dfe738df8722296d06b96c03b151e8cad34
Karmacsi Tamás <83609195+KarmacsiT@users.noreply.github.com>
2025-08-15 04:01:03 +0200

    Add stock card redirect

--- stock-seeker/src/app/ui/home/stock-card.tsx
+++ stock-seeker/src/app/ui/home/stock-card.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import router from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function StockCard({
 	stock: { symbol, name },
 }: {
 	stock: { symbol: string; name: string };
 }) {
+	const { push } = useRouter();
+
 	return (
-		<button className="bg-[#1E1E1E] rounded-2xl p-5 text-center shadow-md hover:shadow-lg hover:bg-[#2A2A2A] transition">
+		<button
+			className="bg-[#1E1E1E] rounded-2xl p-5 text-center shadow-md hover:shadow-lg hover:bg-[#2A2A2A] transition"
+			onClick={() => push(`/search?symbol=${symbol}`)}
+		>
 			<p className="text-lg font-bold text-white">{symbol}</p>
 			<p className="text-sm text-[#CFD8DC]">{name}</p>
 		</button>
